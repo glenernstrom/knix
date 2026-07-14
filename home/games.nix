b3b7d05b17476444205beca5f0e49fc2e913ac27
Glen Ernstrom <otterbrain@protonmail.com>
2026-07-14 18:42:49 -0400

    Fixed typo in games.nix

--- home/games.nix
+++ home/games.nix
@@ -1,10 +1,11 @@
 { config, pkgs , ... }:
 
- programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
- };
+{
+   programs.steam = {
+     enable = true;
+     remotePlay.openFirewall = true;
+     dedicatedServer.openFirewall = true;
+   };
 
  home.packages = with pkgs; [
   kdePackages.kpat
